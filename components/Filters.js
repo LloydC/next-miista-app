4480bfcb3b5ddf7b7378dc1fdeab16788b9852ae
Lloyd Chambrier <lloydchambrier@gmail.com>
2021-05-27 21:12:18 +0100

    Removed unecessary code

--- components/Filters.js
+++ components/Filters.js
@@ -1,7 +1,3 @@
-// import the list of filters to be rendered through an appropriate component
-// import the list of products using useState
-// build one/several functions that will be handling the filtering  of products 
-// import Select from 'react-select';
 import Select from './Select';
 
 const Filters = ({ filters, setFilters }) => {
@@ -10,26 +6,21 @@ const Filters = ({ filters, setFilters }) => {
     <div
       className="filters-container"
     >
-        <h4>Filters</h4>
-        <br/>
         <div style={{display: 'flex'}}> 
 
           <Select
             values={["Color", "White", "Brown", "Green"]}
             onChange={e => setFilters({ ...filters, color: e.target.value === 'color' ? '' : e.target.value })}
-            // options={options}
           />
 
           <Select
             values={["Category", "Flats", "Mules","Sandals", "Outlet","Mid-Heels","E8 Flats", "E8 Mules","E8 Sandals", "New Arrivals"]}
             onChange={e => setFilters({ ...filters, category: e.target.value === 'category' ? '' : e.target.value  })}
-            // options={options}
           />
 
           <Select
             values={["Price", "0-100", "100-500","500-1000"]}
             onChange={e => setFilters({ ...filters, price: e.target.value === 'price' ? '' : e.target.value  })}
-            // options={options}
           />
 
         </div>
